7789cc9b441924f5afa71e448e8cf5fb1afdfacc
treisse93 <misstresse.slt@hotmail.fr>
2023-11-20 14:41:04 +0100

    modif fichiers filter et search, ajout lint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,15 +3,15 @@ import {
   DisplayRecipes,
   UpdateRecipes,
   Summarize,
-} from "../controllers/RecipeControl.js";
-import { GetAllFilters, UpdateFilters } from "../utils/filter.js";
-import { SearchFromMain } from "../utils/search.js";
+} from '../controllers/RecipeControl.js';
+import {GetAllFilters, UpdateFilters} from '../utils/filter.js';
+import {SearchFromMain} from '../utils/search.js';
 import {
   recipesArray,
   ingredientsObject,
   appliancesObject,
   ustensilesObject,
-} from "../controllers/DataControl.js";
+} from '../controllers/DataControl.js';
 
 // Tableau qui contient tous les objets ingrédients, appareils et ustensiles
 const fullArray = [ingredientsObject, appliancesObject, ustensilesObject];
@@ -28,37 +28,47 @@ function init() {
   DisplayRecipes(recipesArray); // Affiche toutes les recettes au chargement de la page
   GetAllFilters(fullArray); // Création: filtres de recherche.
 
+  // Stocker le contenu initial de recipesCardsContainer
+  const initialRecipesContainerHTML = document.getElementById("recipesCardsContainer").innerHTML;
+
   mainInput.addEventListener("keyup", (event) => {
     console.log (mainInput.value);
     // créer une constante mainInput.value pour récupérer la valeur
     const updatedFromMain = SearchFromMain(mainInput.value, recipesArray); 
     // Récupère: filtres correspondant à la recherche.
-
+    
+    const recipeContainer = document.getElementById("recipesCardsContainer");
     if (mainInput.value.length > 2) {
       // Si valeur de l'input recherche > à 2 caractères, affiche les recettes qui correspondent.
-
-      if (updatedFromMain.length > 0) {
+      if (updatedFromMain.length === 0) {
         // Si aucune recette ne correspond, affiche: message d'erreur.
-        const labelsContainer = document.getElementById("labelsContainer");
-        labelsContainer.innerHTML = ""; // Supprime: filtres de recherche.
-        UpdateRecipes(updatedFromMain); // Maj les recettes.
-      } else {
-        const recipeContainer = document.getElementById("recipesCardsContainer");
-        recipeContainer.innerHTML = `<p id='errorMsg>
-                                        Aucune recettes ne correspond à <span id = "output"></span> 
+        console.log (SearchFromMain);
+        recipeContainer.innerHTML = `<p id='errorMsg'>
+                                        Aucune recette ne correspond à <span>${mainInput.value}</span>, 
                                         vous pouvez chercher « tarte aux pommes », « poisson », 
                                         etc.</p>`;
-
-        const output = document.querySelector("#output");
-        output.textContent = mainInput.value;
-
+console.log (recipeContainer);
+console.log (updatedFromMain);
+console.log (updatedFromMain.length === 0);
+        //const output = document.getElementById("output");
+       // output.textContent = mainInput.value;
+        const labelsContainer = document.getElementById("labelsContainer");
+        labelsContainer.innerHTML = ""; // Supprime: filtres de recherche.
+        } else {
+          //Affiche les recettes correspondantes
         Summarize(); // Affiche: résumé nombre de recettes.
         UpdateFilters(updatedFromMain); // Crée: filtres de recherche.
+        UpdateRecipes(updatedFromMain); // Maj les recettes.
       }
     } else {
       // Si la valeur de l'input < à 3 caractères, affiche: toutes les recettes.
       DisplayRecipes(recipesArray);
       GetAllFilters(fullArray);
+      
+    }
+    // Restaurer le contenu initial si nécessaire
+    if (updatedFromMain.length !== 0) {
+      recipeContainer.innerHTML = initialRecipesContainerHTML;
     }
   });
 }
