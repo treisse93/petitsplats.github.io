68cba4183e715ad325e461b27f37308312ce504c
treisse93 <misstresse.slt@hotmail.fr>
2023-11-25 21:20:19 +0100

    correction e-slint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,11 @@
 // Importation des modules depuis d'autres fichiers
 import {
-  DisplayRecipes,
-  UpdateRecipes,
-  Summarize,
+  displayRecipes,
+  updateRecipes,
+  summarize,
 } from '../controllers/RecipeControl.js';
-import {GetAllFilters, UpdateFilters} from '../utils/filter.js';
-import {SearchFromMain} from '../utils/search.js';
+import {getAllFilters, updateFilters} from '../utils/filter.js';
+import {searchFromMain} from '../utils/search.js';
 import {
   recipesArray,
   ingredientsObject,
@@ -21,50 +21,49 @@ const fullArray = [ingredientsObject, appliancesObject, ustensilesObject];
 
 /**
  * Fonction d'initialisation de l'application.
+ *
  */
 // recherche dans la barre principale
 function init() {
-  const mainInput = document.getElementById("bannerSearchInput");
-  DisplayRecipes(recipesArray); // Affiche toutes les recettes au chargement de la page
-  GetAllFilters(fullArray); // Création: filtres de recherche.
+  const mainInput = document.getElementById('bannerSearchInput');
+  displayRecipes(recipesArray);
+  // Affiche toutes les recettes au chargement de la page
+  getAllFilters(fullArray); // Création: filtres de recherche.
 
   // Stocker le contenu initial de recipesCardsContainer
-  const initialRecipesContainerHTML = document.getElementById("recipesCardsContainer").innerHTML;
+  const initialRecipesContainerHTML =
+  document.getElementById('recipesCardsContainer').innerHTML;
+  // verif si pas de plantage
 
-  mainInput.addEventListener("keyup", (event) => {
-    console.log (mainInput.value);
+  mainInput.addEventListener('keyup', (event) => {
     // créer une constante mainInput.value pour récupérer la valeur
-    const updatedFromMain = SearchFromMain(mainInput.value, recipesArray); 
+    const updatedFromMain = searchFromMain(mainInput.value, recipesArray);
     // Récupère: filtres correspondant à la recherche.
-    
-    const recipeContainer = document.getElementById("recipesCardsContainer");
+
+    const recipeContainer = document.getElementById('recipesCardsContainer');
     if (mainInput.value.length > 2) {
-      // Si valeur de l'input recherche > à 2 caractères, affiche les recettes qui correspondent.
+      // Si valeur de l'input recherche > à 2 caractères,
+      // affiche les recettes qui correspondent.
       if (updatedFromMain.length === 0) {
         // Si aucune recette ne correspond, affiche: message d'erreur.
-        console.log (SearchFromMain);
         recipeContainer.innerHTML = `<p id='errorMsg'>
-                                        Aucune recette ne correspond à <span>${mainInput.value}</span>, 
-                                        vous pouvez chercher « tarte aux pommes », « poisson », 
-                                        etc.</p>`;
-console.log (recipeContainer);
-console.log (updatedFromMain);
-console.log (updatedFromMain.length === 0);
-        //const output = document.getElementById("output");
-       // output.textContent = mainInput.value;
-        const labelsContainer = document.getElementById("labelsContainer");
-        labelsContainer.innerHTML = ""; // Supprime: filtres de recherche.
-        } else {
-          //Affiche les recettes correspondantes
-        Summarize(); // Affiche: résumé nombre de recettes.
-        UpdateFilters(updatedFromMain); // Crée: filtres de recherche.
-        UpdateRecipes(updatedFromMain); // Maj les recettes.
+                                      Aucune recette ne correspond à 
+                                      <span>${mainInput.value}</span>, 
+                                      vous pouvez chercher 
+                                      « tarte aux pommes », « poisson », 
+                                      etc.</p>`;
+        const labelsContainer = document.getElementById('labelsContainer');
+        labelsContainer.innerHTML = ''; // Supprime: filtres de recherche.
+      } else {
+        // Affiche les recettes correspondantes
+        summarize(); // Affiche: résumé nombre de recettes.
+        updateFilters(updatedFromMain); // Crée: filtres de recherche.
+        updateRecipes(updatedFromMain); // Maj les recettes.
       }
     } else {
       // Si la valeur de l'input < à 3 caractères, affiche: toutes les recettes.
-      DisplayRecipes(recipesArray);
-      GetAllFilters(fullArray);
-      
+      displayRecipes(recipesArray);
+      getAllFilters(fullArray);
     }
     // Restaurer le contenu initial si nécessaire
     if (updatedFromMain.length !== 0) {
--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,147 +1,191 @@
 // recherche depuis la barre de recherche principale
-function SearchFromMain(ValueToSearch, recipes) {
+/**
+ *
+ * @param {string} ValueToSearch
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromMain(ValueToSearch, recipes) {
   return recipes.filter((recipe) => {
-    const { ingredients, name, description } = recipe;
+    const {ingredients, name, description} = recipe;
     const ToCheck = [
       name,
       description,
       ...ingredients.map((ing) => ing.ingredient),
     ];// affiche nom recette , description, ingrédients
     return ToCheck.some((element) =>
-      Normalized(element).match(Normalized(ValueToSearch))
+      normalized(element).match(normalized(ValueToSearch)),
     );
-      });
-      }
+  });
+}
 
 // recherche depuis la barre de recherche du filtre ingredient
-function SearchFromIngredients(ValueToSearch, Actuals, recipes) {
-  const normalizedKeyword = Normalized(ValueToSearch);  
+/**
+ *
+ * @param {string} ValueToSearch
+ * @param {string} Actuals
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromIngredients(ValueToSearch, Actuals, recipes) {
+  const normalizedKeyword = normalized(ValueToSearch);
   return recipes
-    .filter((recipe) =>
-      recipe.ingredients.some(
-        (ingr) => Normalized(ingr.ingredient) === normalizedKeyword
+      .filter((recipe) =>
+        recipe.ingredients.some(
+            (ingr) => normalized(ingr.ingredient) === normalizedKeyword,
+        ),
       )
-    )
-    .filter((recipe) =>
-      Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id))
-    );
-    }
-    
+      .filter((recipe) =>
+        Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id)),
+      );
+}
 
-//recherche depuis la barre de recherche du filtre ustensiles
-function SearchFromUstensils(ValueToSearch, Actuals, recipes) {
-  const normalizedKeyword = Normalized(ValueToSearch);
+
+// recherche depuis la barre de recherche du filtre ustensiles
+/**
+ *
+ * @param {string} ValueToSearch
+ * @param {string} Actuals
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromUstensils(ValueToSearch, Actuals, recipes) {
+  const normalizedKeyword = normalized(ValueToSearch);
 
   return recipes
-    .filter((recipe) =>
-      recipe.ustensils.some(
-        (Ustensil) => Normalized(Ustensil) === normalizedKeyword
+      .filter((recipe) =>
+        recipe.ustensils.some(
+            (Ustensil) => normalized(Ustensil) === normalizedKeyword,
+        ),
       )
-    )
-    .filter((recipe) =>
-      Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id))
-    );
-    
-
+      .filter((recipe) =>
+        Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id)),
+      );
 }
 
 // recherche depuis la barre de recherche du filtre appareils
-function SearchFromAppliances(ValueToSearch, Actuals, recipes) {
-  const normalizedKeyword = Normalized(ValueToSearch);
+/**
+ *
+ * @param {string} ValueToSearch
+ * @param {string} Actuals
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromAppliances(ValueToSearch, Actuals, recipes) {
+  const normalizedKeyword = normalized(ValueToSearch);
   return recipes
-    .filter((recipe) => Normalized(recipe.appliance) === normalizedKeyword)
-    .filter((recipe) =>
-      Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id))
-    );
-    
+      .filter((recipe) => normalized(recipe.appliance) === normalizedKeyword)
+      .filter((recipe) =>
+        Actuals.some((Recipe) => Number(Recipe.id) === Number(recipe.id)),
+      );
 }
 
-//selection depuis la barre de recherche du filtre 
-function SearchFromFilter(ValueToSearch, filterZone, recipes) {
-  const Actuals = Array.from(document.querySelectorAll(".recipeCard"));
+// selection depuis la barre de recherche du filtre
+/**
+ *
+ * @param {string} ValueToSearch
+ * @param {string} filterZone
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromFilter(ValueToSearch, filterZone, recipes) {
+  const Actuals = Array.from(document.querySelectorAll('.recipeCard'));
   let UpdatedRecipes;
 
-  if (filterZone === "ingredients") {
-    UpdatedRecipes = SearchFromIngredients(ValueToSearch, Actuals, recipes);
-  } else if (filterZone === "appliances") {
-    UpdatedRecipes = SearchFromAppliances(ValueToSearch, Actuals, recipes);
-  } else if (filterZone === "ustensils") {
-    UpdatedRecipes = SearchFromUstensils(ValueToSearch, Actuals, recipes);
+  if (filterZone === 'ingredients') {
+    UpdatedRecipes = searchFromIngredients(ValueToSearch, Actuals, recipes);
+  } else if (filterZone === 'appliances') {
+    UpdatedRecipes = searchFromAppliances(ValueToSearch, Actuals, recipes);
+  } else if (filterZone === 'ustensils') {
+    UpdatedRecipes = searchFromUstensils(ValueToSearch, Actuals, recipes);
     console.log(Actuals);
   }
   return UpdatedRecipes;
 }
 
-function SearchFromDeleteLabel(recipes) {
-  // une fonction qui recuperes les labels et renvoi que les recettes qui contiennent l'ensemble des labels
-  const ActualsLabel = Array.from(document.querySelectorAll(".labels"));
-  let iteration = 0;
+/**
+ *
+ * @param {string} recipes
+ * @return {string}
+ */
+function searchFromDeleteLabel(recipes) {
+  // une fonction qui recuperes les labels et
+  // renvoi que les recettes qui contiennent l'ensemble des labels
+  const ActualsLabel = Array.from(document.querySelectorAll('.labels'));
+  // let iteration = 0;
   let updatedRecipes = recipes;
 
   ActualsLabel.forEach((label) => {
-    const name = label.getAttribute("data-normalized");
-    const type = label.getAttribute("data-type");
+    const name = label.getAttribute('data-normalized');
+    const type = label.getAttribute('data-type');
 
-    if (type === "ingredients") {
-      updatedRecipes = SearchFromIngredients(name, updatedRecipes, recipes);
+    if (type === 'ingredients') {
+      updatedRecipes = searchFromIngredients(name, updatedRecipes, recipes);
       iteration += 1;
-    } else if (type === "ustensils") {
-      updatedRecipes = SearchFromUstensils(name, updatedRecipes, recipes);
+    } else if (type === 'ustensils') {
+      updatedRecipes = searchFromUstensils(name, updatedRecipes, recipes);
       iteration += 1;
-    } else if (type === "appliances") {
-      updatedRecipes = SearchFromAppliances(name, updatedRecipes, recipes);
+    } else if (type === 'appliances') {
+      updatedRecipes = searchFromAppliances(name, updatedRecipes, recipes);
       iteration += 1;
     }
   });
   return updatedRecipes;
 }
 
-function SearchListInput(filters, input) {
+/**
+ *
+ * @param {*} filters
+ * @param {*} input
+ */
+function searchListInput(filters, input) {
   // Fonction qui filtre les éléments de la liste des filtres
 
-  if (input !== 0) {  
+  if (input !== 0) {
     for (const filter of filters) {
       const element = filter;
-   
-      const normalizedElement = Normalized(element.textContent); // nom de l'élément du filtre
-      const normalizedInput = Normalized(input); //valeur du champ input du filtre recherche
-console.log (normalizedElement);
-console.log (normalizedInput);
-console.log(!normalizedElement.match(normalizedInput)); 
-      if (!normalizedElement.match(normalizedInput)) { //true ou false
-        console.log(" ça passe pas");
-        element.classList.add("d-none");
-        element.classList.remove("d-flex");
+      const normalizedElement = normalized(element.textContent);
+      // nom de l'élément du filtre
+      const normalizedInput = normalized(input);
+      // valeur du champ input du filtre recherche
+      if (!normalizedElement.match(normalizedInput)) { // true ou false
+        element.classList.add('d-none');
+        element.classList.remove('d-flex');
       } else {
-        console.log ("ca passe");
-        element.classList.remove("d-none");
-        element.classList.add("d-flex");
+        element.classList.remove('d-none');
+        element.classList.add('d-flex');
       }
     }
   }
 }
 
-
-
-// Fonction utilitaire pour normaliser les chaînes de caractères (enlever les accents et convertir en minuscules)
-function Normalized(str) {
+// Fonction utilitaire pour normaliser les chaînes de caractères
+// (enlever les accents et convertir en minuscules)
+/**
+ *
+ * @param {str}
+ * @return {string}
+ */
+function normalized(str) {
   if (typeof str === 'string' || str instanceof String) {
     return str
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replaceAll(" ", "")
-      .replace("'", "")
-      .toLowerCase()
-      .trim();
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replaceAll(' ', '')
+        .replace('', '')
+        .toLowerCase()
+        .trim();
   } else {
-    return ""; // Retourne une chaîne vide si la valeur n'est pas une chaîne de caractères
+    return '';
+    // Retourne une chaîne vide si la valeur
+    // n'est pas une chaîne de caractères
   }
 }
 
 export {
-  SearchFromFilter,
-  SearchFromMain,
-  SearchListInput,
-  SearchFromDeleteLabel,
-  Normalized,
+  searchFromFilter,
+  searchFromMain,
+  searchListInput,
+  searchFromDeleteLabel,
+  normalized,
 };
